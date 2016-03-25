3f7a2ec6afa75622eb7479c223a3209e4f0e014e
Brandon Myers <bdmyers@cs.washington.edu>
2016-03-25 15:56:53 -0700

    more wget bad stuff

--- grappa/install_dependencies.sh
+++ grappa/install_dependencies.sh
@@ -17,7 +17,7 @@ apt-get install -y \
 
 # cmake 3.12+
 cd /root
-wget https://cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.sh && \
+wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.sh && \
    chmod +x cmake-3.5.1-Linux-x86_64.sh && \
    ./cmake-3.5.1-Linux-x86_64.sh
 
