de74573835f27c747de31b8ea02dab6003bbb271
Brandon Myers <bdmyers@cs.washington.edu>
2016-03-25 17:32:11 -0700

    dont use third-party yet

--- grappa/env.sh
+++ grappa/env.sh
@@ -1,7 +1,7 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
-export GRAPPA_THIRD_PARTY=/grappa-third-party
+#export GRAPPA_THIRD_PARTY=/grappa-third-party
 export GRAPPA_ALLOW_RUN_AS_ROOT=1
 
 echo 'export PATH=$HOME/cmake-3.5.1-Linux-x86_64/bin:$PATH' >>/home/grappa/.bashrc
